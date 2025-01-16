82316a33e2061dfbb00a63daec29a2e9ed74012d
Abin Simon <mail@meain.io>
2025-01-16 11:47:23 +0530

    Add title to markdown tempalte

--- log.go
+++ log.go
@@ -92,10 +92,11 @@ func formatPRSmall(p pr, url string) string {
 
 func formatPRMarkdown(p pr, url string) string {
 	line := fmt.Sprintf(
-		"- [%d](%s/pull/%d)",
+		"- [%d](%s/pull/%d) %s",
 		p.number,
 		url,
 		p.number,
+		p.title,
 	)
 
 	return line
